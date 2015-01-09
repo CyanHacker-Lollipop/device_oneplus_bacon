c64b3a65b328a36ef5bda6c9126566f1f5140d0b
noname55804 <marlin2201@gmail.com>
2015-01-09 17:37:00 +0100

    bacon: change to vendor/ch
    Change-Id: I5b92be00d61584fa072d235a7a6e49192d44ee42
    Signed-off-by: noname55804 <marlin2201@gmail.com>

--- cm.mk
+++ cm.mk
@@ -23,10 +23,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/ch/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/ch/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_bacon
 PRODUCT_DEVICE := bacon
