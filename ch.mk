81cd9f768b63796f375414af96425bd786072d07
yordiderijcke <yordi.derijcke@live.be>
2015-05-03 13:33:42 +0200

    Update Optimizations
    Change-Id: Ib97b3aec4354c445b8e472a516ff9fd9f5e90c6d
    Signed-off-by: yordiderijcke <yordi.derijcke@live.be>

--- ch.mk
+++ ch.mk
@@ -17,6 +17,16 @@
 USE_OPTIMIZATIONS := true
 
 ifneq ($(strip $(USE_OPTIMIZATIONS)),false)
+include vendor/ch/config/sm_clear_vars.mk
+# Find host OS
+UNAME := $(shell uname -s)
+
+ifeq ($(strip $(UNAME)),Linux)
+  HOST_OS := linux
+endif
+
+# Only use these compilers on linux host.
+ifeq ($(strip $(HOST_OS)),linux)
 #HACKIFY CONFIGURATION 
 
 ##Define ROM toolchain
@@ -31,44 +41,21 @@ TARGET_GCC_ARM := 4.9-sm
 ##Enable Pthread (only on newer devices)
 ENABLE_PTHREAD := true
 
-##Enable O3 Optimizations
-O3_OPTIMIZATIONS:= true
-
 ##Use Kernel Optimizations?
 USE_KERNEL_OPTIMIZATIONS := true
-
-##Use Extra Optimizations?
-USE_EXTRA_OPTIMIZATIONS := true
 
 ##How many threads does the device have?
 PRODUCT_THREADS := 4
 
-ifeq ($(strip $(USE_EXTRA_OPTIMIZATIONS)),true)
-# Extra SaberMod GCC C flags for the ROM and Kernel
-export EXTRA_SABERMOD_GCC_CFLAGS := \
-         -ftree-loop-distribution \
-         -ftree-loop-if-convert \
-         -ftree-loop-im \
-         -ftree-loop-ivcanon \
-         -fprefetch-loop-arrays \
-         -ftree-vectorize \
-         -mvectorize-with-neon-quad \
-         -pipe
-
-# Extra SaberMod CLANG C flags
-EXTRA_SABERMOD_CLANG_CFLAGS := \
-  -fprefetch-loop-arrays \
-  -ftree-vectorize \
-  -pipe
-
-OPT4 := -extra
-
 GRAPHITE_KERNEL_FLAGS := \
-  -floop-parallelize-all \
-  -ftree-parallelize-loops=$(PRODUCT_THREADS) \
-  -fopenmp
+    -floop-parallelize-all \
+    -ftree-parallelize-loops=$(PRODUCT_THREADS) \
+    -fopenmp
 
-endif
+# Extra SaberMod GCC C flags for arch target and Kernel
+export EXTRA_SABERMOD_GCC_VECTORIZE_CFLAGS := \
+         -ftree-vectorize \
+         -mvectorize-with-neon-quad
 
 endif
 #################################################
